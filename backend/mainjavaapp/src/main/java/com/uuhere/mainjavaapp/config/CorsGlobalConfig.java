172ec800be56c39c41f3514ccd86c2e342779870
Umesh Unnikrishnan <umeshvu@outlook.com>
2025-07-11 16:12:52 +0530

    frontend changes

---
+++ backend/mainjavaapp/src/main/java/com/uuhere/mainjavaapp/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.uuhere.mainjavaapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration cors = new CorsConfiguration();
+//        cors.addAllowedOrigin("*"); // Or specify: "http://localhost:3000"
+        cors.addAllowedOrigin("http://localhost:3000");
+        cors.addAllowedHeader("*");
+        cors.addAllowedMethod("*");
+        cors.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+
+        return new CorsWebFilter(source);
+    }
+}
+
+
